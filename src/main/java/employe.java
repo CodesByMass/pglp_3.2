72f56891aab84e5b010bc539f8d8a80a2918348b
Massyl Selmi <mass.selmi@gmail.com>
2020-02-26 11:48:59 +0100

    exercice 1

---
+++ src/main/java/employe.java
@@ -0,0 +1,17 @@
+
+public class employe extends allEmploye {
+	final private int salaire = 1500 ;
+	private int startYear;
+	public employe (int startYear) {
+	}
+	
+	@Override
+	public double salaire() {
+		// TODO Auto-generated method stub
+		return  salaire ;
+	}
+	
+	
+	
+
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+	
+	}
+
+}
